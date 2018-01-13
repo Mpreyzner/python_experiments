9a3e786d36bfb9d7ea6d7c1592326a7621717287
Mpreyzner <maria.preyzner@gmail.com>
2018-01-13 12:28:15 +0100

    Add simple file explorer

--- 16_simple_file_explorer.py
+++ 16_simple_file_explorer.py
@@ -2,26 +2,24 @@ import os
 # list folders
 # move folder up
 # move folder down
-# display text content of file
 
 
-cwd = os.getcwd()
-
-
-# os.chdir(downloadDir)
-
-
-for filename in os.listdir(cwd):
-    absPath = (os.path.join(os.getcwd(), filename))
-    extension = os.path.splitext(filename)[1].lstrip('.').lower()
-    print(filename)
+def print_files(dir):
+    for filename in os.listdir(cwd):
+        abs_path = (os.path.join(os.getcwd(), filename))
+        extension = os.path.splitext(filename)[1].lstrip('.').lower()
+        print(filename)
 
 
 while True:
+    cwd = os.getcwd()
+    relative_path = os.path.dirname(__file__)
     print(cwd)
-    choice = input('If you want to move up enter u if down press d')
+    print(print_files(cwd))
+    choice = input('If you want to move up enter .. if you want to move down enter folder name')
     print(choice)
-    if choice not in ['u', 'd']:
-        raise Exception('u or d silly')
-    # os.chdir()
-# https://2.bp.blogspot.com/-PvgHh2ndj3U/WjbPr28XAnI/AAAAAAAAVOc/phMNmZDiUioUbkTN9zLmEg_YVwHBv3IogCLcBGAs/s1600/100%2Bpomys%25C5%2582%25C3%25B3w%2Bpython.png
+    if choice == '..':
+        os.chdir('..' + relative_path)
+    elif choice.isprintable():
+        os.chdir(cwd + '/' + choice)
+
